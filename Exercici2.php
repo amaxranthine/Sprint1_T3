76168ff7bfd9c94ab922ba7fb2d36c2ec8a6f525
Laura <amaxranthine1995@gmail.com>
2024-02-21 13:47:39 +0100

    empezar arrays php

---
+++ Exercici2.php
@@ -0,0 +1,13 @@
+/** $X = array (10, 20, 30, 40, 50,60);
+Mostrar per pantalla la mida de l’array anterior -- count()
+i posteriorment elimina un element de l’array anterior.--array_pop()
+ Després d'eliminar l'element, les claus senceres han de ser normalitzades
+ (s’han de reorganitzar els seus índexs).-- array_values()
+ Mostra per última vegada la mida de l’array.
+ */
+ <?php
+$x = array (10, 20, 30, 40, 50,60);
+$mida = count($x);
+    echo $mida;
+
+ ?>
