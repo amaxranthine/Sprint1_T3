97f8258e40e4b57b35aad5b64c0805024e3e9be8
Laura <amaxranthine1995@gmail.com>
2024-03-13 13:34:45 +0100

    creo que se logro

--- Exercici3.php
+++ Exercici3.php
@@ -1,10 +1,11 @@
+<?php
 /*Crea una funció que rebi com a paràmetres un array de paraules i un caràcter.
  La funció ens retorna true si totes les paraules de array tenen el caràcter 
  passat com a segon paràmetre.
 Per exemple:
 Si tenim [“hola”, “Php”, “Html”] retornarà true si preguntem per “h” però fals si preguntem per “l”.*/
 
-<?php
+
 $paraules = ["hola", "Php", "Html"];
 $caracter = "h";
 
@@ -18,7 +19,7 @@ echo "Lletra a buscar: $caracter" . "</br>";
 echo "</br>";
 
 /** Llamar a la funcion busqueda. Los mensajes correspondientes */
-$resultat = busqueda ($caracter, $paraules);
+$resultat = busqueda ($caracter , $paraules);
 if($resultat == true){
     $resposta = "S'ha trobat la lletra $caracter a totes les paraules";
 } else{
