8c6766ad9344135578de4fd7839a1b22e3ee4f45
Laura <amaxranthine1995@gmail.com>
2024-04-02 10:41:38 +0200

    imprimir

--- Nivel2/ex1.php
+++ Nivel2/ex1.php
@@ -8,10 +8,18 @@
 
 $numEntero1 = [1,3,4,6,9];
 $numEntero2 =[1,2,5,6,10];
+//array_intersect — Calcula la intersección de arrays
+ $numIgual = array_intersect ($numEntero1, $numEntero2);
+ //combinar las dos arrays: array_merge
+ $numMerge = array_merge($numEntero1, $numEntero2);
+ 
+ //imprimir los resultados
 
-
-
-
+ echo "Array 1:" . "</br>";
+ print_r($numEntero1);
+ echo  "</br>";
+ 
+ echo "Array 2:" . "</br>";
 
 
 
